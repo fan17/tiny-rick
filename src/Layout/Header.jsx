f4ca35204ab3d80a872f32f8eb3bdd1a0b8ba366
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2018-12-30 12:28:30 +0100

    strt

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Container = styled.header`
+    background-color: red;
+`
 
 class Header extends React.Component {
     render() {
         return (
-            <nav>
+            <Container>
                 <Link to="/">Home</Link>
                 <Link to="/">Episodes</Link>
                 <Link to="/quiz">Quiz</Link>
-            </nav>
+            </Container>
         )
     }
 }
